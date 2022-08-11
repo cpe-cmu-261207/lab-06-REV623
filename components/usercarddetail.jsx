016ddd26acf4e06a4c3dda69bcbb933e6ec8eb1b
Reverse <jedsadakorn_k@cmu.ac.th>
2022-08-11 16:18:39 +0700

    finished

---
+++ components/usercarddetail.jsx
@@ -0,0 +1,17 @@
+import {
+    IconMailForward,
+    IconMapPins,
+} from "@tabler/icons";
+
+export default function UserCardDetail(props){
+    return(
+        <div className="text-center">
+            <p>
+                <IconMailForward /> {props.email}
+            </p>
+            <p>
+                <IconMapPins /> {props.address}
+            </p>
+        </div>
+    )
+}
